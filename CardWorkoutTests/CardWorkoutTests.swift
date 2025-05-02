3d49f2c9ca06fab048da93626921660a9aba3c58
Aung Khant Kyaw <aungkhantkyaw8918.com>
2025-05-02 13:45:01 +0630

    Initial Commit

---
+++ CardWorkoutTests/CardWorkoutTests.swift
@@ -0,0 +1,17 @@
+//
+//  CardWorkoutTests.swift
+//  CardWorkoutTests
+//
+//  Created by Digital Base on 2/5/25.
+//
+
+import Testing
+@testable import CardWorkout
+
+struct CardWorkoutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
